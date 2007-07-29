c13428a04af75d5d780be3acf9bfe427cf44510f
Jamis Buck <jamis@37signals.com>
2007-07-29 05:49:30 +0000

    not quite finished yet, but this is the direction this needs to go
    git-svn-id: http://svn.jamisbuck.org/projects/net-scp/trunk@64 e3b5881a-3c0d-0410-9405-fd48df27f097

--- lib/net/scp.rb
+++ lib/net/scp.rb
@@ -12,219 +12,65 @@ module Net
       self.logger = session.logger
     end
 
-    def local_to_remote(io, remote, options={}, &progress)
-      io = StringIO.new(io) if String === io
-      session.open_channel do |channel|
-        channel.exec "scp -t #{remote}" do |ch, success|
-          if success
-            channel[:io      ] = io
-            channel[:remote  ] = remote
-            channel[:options ] = options
-            channel[:progress] = progress
-            begin_upload(channel)
-          else
-            channel.close
-            raise "could not exec scp on the remote host"
-          end
-        end
-      end
+    def upload(local, remote, options={}, &progress)
+      start_command(:upload, local, remote, options, &progress)
     end
 
-    def mkdir(remote, options={})
-      session.open_channel do |channel|
-        channel.exec "scp -rt #{remote}" do |ch, success|
-          if success
-            channel[:remote ] = remote
-            channel[:options] = options
-            begin_mkdir(channel)
-          else
-            channel.close
-            raise "could not exec scp on the remote host"
-          end
-        end
-      end
-    end
-
-    def remote_to_local(remote, options={}, &callback)
-      session.open_channel do |channel|
-        channel.exec "scp -rf #{remote}" do |ch, success|
-          if success
-            channel[:remote  ] = remote
-            channel[:options ] = options
-            channel[:callback] = callback
-            begin_download(channel)
-          else
-            channel.close
-            raise "could not exec scp on the remote host"
-          end
-        end
-      end
+    def download(remote, local, options={}, &progress)
+      start_command(:download, local, remote, options, &progress)
     end
 
     private
 
-      def prepare_channel(channel)
-        channel[:buffer] = Net::SSH::Buffer.new
-        channel[:state ] = :start
-
-        channel.on_close                  { |ch| raise "SCP did not finish successfully (#{ch[:exit]})" if ch[:exit] != 0 }
-        channel.on_data                   { |ch, data| channel[:buffer].append(data) }
-        channel.on_extended_data          { |ch, type, data| debug { data } }
-        channel.on_request("exit-status") { |ch, data| channel[:exit] = data.read_long; true }
-      end
-
-      def begin_download(channel)
-        prepare_channel(channel)
-        channel[:stack] = []
-        channel.on_process { download_state_machine(channel) }
-      end
-
-      def download_state_machine(channel)
-        case channel[:state]
-        when :start
-          channel[:callback].call(:begin)
-          channel.send_data("\0")
-          channel[:state] = :directive
-        when :directive
-          process_directive(channel)
-        when :read
-          download_read(channel)
-        when :finish_read
-          download_finish_read(channel)
-        when :finish
-          channel[:callback].call(:end)
-          channel.close
-        end
-      end
-
-      def download_read(channel)
-        return if channel[:buffer].empty?
-        data = channel[:buffer].read!(channel[:remaining])
-        channel[:callback].call(:read, channel[:file], data)
-        channel[:remaining] -= data.length
-        if channel[:remaining] == 0
-          channel[:state] = :finish_read
-          download_state_machine(channel)
-        end
-      end
-
-      def download_finish_read(channel)
-        if check_response(channel)
-          channel[:callback].call(:finish, channel[:file])
-          channel[:file] = nil
-          channel[:state] = channel[:stack].empty? ? :finish : :directive
-          channel.send_data("\0")
-        end
-      end
-
-      def begin_mkdir(channel)
-        prepare_channel(channel)
-        channel.on_process { mkdir_state_machine(channel) }
-      end
-
-      def mkdir_state_machine(channel)
-        case channel[:state]
-        when :start
-          mkdir_start_state(channel)
-        when :close
-          mkdir_close_state(channel)
-        when :finish
-          mkdir_finish_state(channel)
-        end
-      end
-
-      def mkdir_start_state(channel)
-        if check_response(channel)
-          mode = channel[:options][:mode] || 0700
-          directive = "D%04o %d %s\n" % [mode, 0, File.basename(channel[:remote])]
-          channel.send_data(directive)
-          channel[:state] = :close
-        end
-      end
-
-      def mkdir_close_state(channel)
-        if check_response(channel)
-          channel.send_data("E\n")
-          channel[:state] = :finish
-        end
-      end
-
-      def mkdir_finish_state(channel)
-        if check_response(channel)
-          # FIXME technically need to send "\0" and wait for response one more time
-          channel.close
-        end
-      end
-
       DEFAULT_CHUNK_SIZE = 2048
 
-      def begin_upload(channel)
-        prepare_channel(channel)
+      def start_command(mode, local, remote, options={}, &callback)
+        session.open_channel do |channel|
+          command = "#{scp_command(mode, options)} #{remote}"
+          channel.exec(command) do |ch, success|
+            if success
+              channel[:local   ] = local
+              channel[:remote  ] = remote
+              channel[:options ] = options
+              channel[:callback] = callback
+              channel[:buffer  ] = Net::SSH::Buffer.new
+              channel[:state   ] = :"#{mode}_start"
+              channel[:stack   ] = []
 
-        channel[:sent      ] = 0
-        channel[:size      ] = channel[:options][:size] || (channel[:io].respond_to?(:size) ? channel[:io].size : channel[:io].stat.size)
+              channel.on_close                  { |ch| raise "SCP did not finish successfully (#{ch[:exit]})" if ch[:exit] != 0 }
+              channel.on_data                   { |ch, data| channel[:buffer].append(data) }
+              channel.on_extended_data          { |ch, type, data| debug { data.chomp } }
+              channel.on_request("exit-status") { |ch, data| channel[:exit] = data.read_long; true }
+              channel.on_process                { send("#{channel[:state]}_state", channel) }
+            else
+              channel.close
+              raise "could not exec scp on the remote host"
+            end
+          end
+        end
+      end
+
+      def scp_command(mode, options)
+        command = "scp "
+        command << (mode == :upload ? "-t" : "-f")
+        command << " -v" if options[:verbose]
+        command << " -r" if options[:recursive]
+        command << " -p" if options[:preserve]
+        command
+      end
+
+      def upload_start_state(channel)
         channel[:chunk_size] = channel[:options][:chunk_size] || DEFAULT_CHUNK_SIZE
-
-        channel.on_process { upload_state_machine(channel) }
+        set_current(channel, channel[:local])
+        await_response(channel, :upload_current)
       end
 
-      def upload_state_machine(channel)
-        case channel[:state]
-        when :start
-          start_state(channel)
-        when :upload
-          upload_state(channel)
-        when :continue
-          send_next_chunk(channel)
-        when :finish
-          channel.close
-        end
+      def download_start_state(channel)
+        channel.send_data("\0")
+        channel[:state] = :read_directive
       end
 
-      def start_state(channel)
-        if check_response(channel)
-          mode = channel[:options][:mode] || 0600
-          size = channel[:options][:size] || (channel[:io].respond_to?(:size) ? channel[:io].size : channel[:io].stat.size)
-          directive = "C%04o %d %s\n" % [mode, size, File.basename(channel[:remote])]
-          channel.send_data(directive)
-          update_progress(channel)
-          channel[:state] = :upload
-        end
-      end
-
-      def upload_state(channel)
-        if check_response(channel)
-          channel[:state] = :continue
-          send_next_chunk(channel)
-        end
-      end
-
-      def send_next_chunk(channel)
-        data = channel[:io].read(channel[:chunk_size])
-        if data.nil?
-          channel.send_data("\0")
-          channel[:state] = :finish
-        else
-          channel[:sent] += data.length
-          channel.send_data(data)
-          update_progress(channel)
-        end
-      end
-
-      def check_response(channel)
-        return false if channel[:buffer].available == 0
-        c = channel[:buffer].read_byte
-        return true if c == 0
-        raise "#{c.chr}#{channel[:buffer].read}"
-      end
-
-      def update_progress(channel)
-        if channel[:progress]
-          channel[:progress].call(channel[:sent], channel[:size])
-        end
-      end
-
-      def process_directive(channel)
+      def read_directive_state(channel)
         return unless line = channel[:buffer].read_to("\n")
         channel[:buffer].consume!
 
@@ -235,19 +81,125 @@ module Net
         when :directory
           channel[:stack] << directive.merge(:times => channel[:times])
           channel[:times] = nil
-          channel[:callback].call(:in, channel[:stack].last)
         when :file
           channel[:file] = directive.merge(:times => channel[:times])
           channel[:times] = nil
           channel[:remaining] = channel[:file][:size]
-          channel[:callback].call(:start, channel[:file])
-          channel[:state] = :read
+          channel[:state] = :read_data
         when :end
-          channel[:callback].call(:out, channel[:stack].pop)
           channel[:state] = :finish if channel[:stack].empty?
         end
 
         channel.send_data("\0")
+      end
+
+      def finish_state(channel)
+        channel.close
+      end
+
+      def read_data_state(channel)
+        return if channel[:buffer].empty?
+        data = channel[:buffer].read!(channel[:remaining])
+        channel[:remaining] -= data.length
+        await_response(channel, :finish_read) if channel[:remaining] == 0
+      end
+
+      def await_response(channel, next_state)
+        channel[:state] = :await_response
+        channel[:next ] = next_state
+        # check right away, to see if the response is immediately available
+        await_response_state(channel)
+      end
+
+      def await_response_state(channel)
+        return if channel[:buffer].available == 0
+        c = channel[:buffer].read_byte
+        raise "#{c.chr}#{channel[:buffer].read}" if c != 0
+        channel[:next], channel[:state] = nil, channel[:next]
+        send(:"#{channel[:state]}_state", channel)
+      end
+
+      def finish_read_state(channel)
+        channel[:file] = nil
+        channel[:state] = channel[:stack].empty? ? :finish : :read_directive
+        channel.send_data("\0")
+      end
+
+      def set_current(channel, path)
+        path = channel[:cwd] ? File.join(channel[:cwd], path) : path
+        channel[:current] = path
+        channel[:stat] = File.stat(path)
+      end
+
+      def next_item_state(channel)
+        next_item = channel[:stack].last.shift
+        if next_item.nil?
+          channel[:stack].pop
+          channel[:cwd] = File.dirname(channel[:cwd])
+          channel.send_data("E\n")
+          await_response(channel, channel[:stack].empty? ? :finish : :next_item)
+        else
+          set_current(channel, next_item)
+          upload_current_state(channel)
+        end
+      end
+  
+      def upload_current_state(channel)
+        if File.directory?(channel[:current])
+          raise ArgumentError, "can't upload directories unless :recursive" unless channel[:options][:recursive]
+          upload_directory_state(channel)
+        elsif File.file?(channel[:current])
+          upload_file_state(channel)
+        else
+          raise ArgumentError, "not a directory or a regular file: #{channel[:current].inspect}"
+        end
+      end
+
+      def upload_directory_state(channel)
+        if preserve_attributes_if_requested(channel)
+          mode = channel[:stat].mode & 07777
+          directive = "D%04o %d %s\n" % [mode, 0, File.basename(channel[:current])]
+          channel.send_data(directive)
+          channel[:cwd] = channel[:current]
+          channel[:stack] << Dir.entries(channel[:current]).reject { |i| i == "." || i == ".." }
+          await_response(channel, :next_item)
+        end
+      end
+
+      def upload_file_state(channel)
+        if preserve_attributes_if_requested(channel)
+          mode = channel[:stat].mode & 07777
+          directive = "C%04o %d %s\n" % [mode, channel[:stat].size, File.basename(channel[:current])]
+          channel.send_data(directive)
+          channel[:io] = File.open(channel[:current], "rb")
+          await_response(channel, :send_data)
+        end
+      end
+
+      def preserve_attributes_if_requested(channel)
+        if channel[:options][:preserve] && !channel[:preserved]
+          channel[:preserved] = true
+          stat = channel[:stat]
+          directive = "T%d %d %d %d\n" % [stat.mtime.to_i, stat.mtime.usec, stat.atime.to_i, stat.atime.usec]
+          channel.send_data(directive)
+          type = stat.directory? ? :directory : :file
+          await_response(channel, :"upload_#{type}")
+          return false
+        else
+          channel[:preserved] = false
+          return true
+        end
+      end
+
+      def send_data_state(channel)
+        data = channel[:io].read(channel[:chunk_size])
+        if data.nil?
+          channel[:io].close
+          channel.send_data("\0")
+          await_response(channel, :next_item)
+        else
+          channel.send_data(data)
+        end
       end
 
       def parse_directive(text)
