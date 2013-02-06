785dbd6c29a46c79e343a6da3b882f736531bdbc
delano <delano.mandelbaum@gmail.com>
2013-02-06 10:51:19 -0800

    Version bump

--- lib/net/scp/version.rb
+++ lib/net/scp/version.rb
@@ -6,7 +6,7 @@ module Net; class SCP
   class Version < Net::SSH::Version
     MAJOR = 1
     MINOR = 0
-    TINY  = 5
+    TINY  = 6
 
     # The current version, as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
