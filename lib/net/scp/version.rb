3e63e9f57914b072402f1433a0973aa1854bd144
delano <delano.mandelbaum@gmail.com>
2013-02-06 12:45:46 -0800

    Dep fix to stop the madness

--- lib/net/scp/version.rb
+++ lib/net/scp/version.rb
@@ -5,8 +5,8 @@ module Net; class SCP
   # Describes the current version of the Net::SCP library.
   class Version < Net::SSH::Version
     MAJOR = 1
-    MINOR = 0
-    TINY  = 6
+    MINOR = 1
+    TINY  = 0
 
     # The current version, as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
